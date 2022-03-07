897c0024aa056fcf212d6e32f2b45bd0d597d371
Ludmila <Ludiky30@gmail.com>
2022-03-07 22:38:35 +0700

    task 2-3

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const products = [
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = ({title = 'untitled', price = 0}) => {
     return `<div class="product-item">
                 <h3 class="product-item__title">${title}</h3>
                 <p class="product-item__price">${price}</p>
@@ -14,9 +14,15 @@ const renderProduct = (title, price) => {
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
+    const productsList = list.map(item => renderProduct(item));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    productsList.forEach(elem => {
+        document.querySelector('.products').innerHTML += elem;
+    });
 };
+
+/* Ответ на задание 3.
+Запятая появлялась по причине того, что в свойство innerHTML мы помещали массив.
+Для решения этой проблемы нужно перебрать массив и добавить к innerHTML каждый из элементов. */
 
 renderPage(products);
