7d3612259243b1fbe28a0968ed76aa8e9cb2552e
Ludmila <Ludiky30@gmail.com>
2022-03-08 16:57:00 +0700

    fixed task 3

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ const products = [
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = ({title = 'untitled', price = 0}) => {
+const renderProduct = ({title = 'untitled', price = 0}, image='./img/no-image.jpg') => {
     return `<div class="product-item">
+                <img class="product-item__photo" src="${image}" alt="${title}"> 
                 <h3 class="product-item__title">${title}</h3>
                 <p class="product-item__price">${price}</p>
                 <button class="buy-btn">Купить</button>
@@ -16,13 +17,13 @@ const renderProduct = ({title = 'untitled', price = 0}) => {
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item));
     console.log(productsList);
-    productsList.forEach(elem => {
-        document.querySelector('.products').innerHTML += elem;
-    });
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 /* Ответ на задание 3.
 Запятая появлялась по причине того, что в свойство innerHTML мы помещали массив.
-Для решения этой проблемы нужно перебрать массив и добавить к innerHTML каждый из элементов. */
+Для решения этой проблемы можно перебрать массив и добавить к innerHTML каждый из элементов. 
+Но если товаров много, такой способ будет не очень продуктивным. Поэтому просто с помощью join 
+склеим массив строк в одну строку */
 
 renderPage(products);
